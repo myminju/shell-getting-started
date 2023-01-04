a2adb7b0862c230f6fbad0468bc1ea07d5cea507
Minju Jeon <mjeon7890@gmail.com>
2023-01-04 21:07:17 +0900

    add: craete a simple calculator

---
+++ cal.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo "====== Simple Calculator ======="
+
+total=
+read -p "Input Number : " num1
+read -p "Input Operation : " op
+read -p "Input Number : " num2
+
+case $op in
+  '+') total=`expr $num1 + $num2` ;;
+  '-') total=`expr $num1 - $num2` ;;
+  '*') total=`expr $num1 * $num2` ;;
+  '/') total=`expr $num1 / $num2` ;;
+esac
+
+read -p "Will you stop? " answer
+
+if [ $answer = "NO" ]
+then
+  while true
+  do
+    read -p "Input Operation : " op
+    read -p "Input Number : " num2
+    
+    case $op in
+      '+') total=`expr $num1 + $num2` ;;
+      '-') total=`expr $num1 - $num2` ;;
+      '*') total=`expr $num1 \* $num2` ;;
+      '/') total=`expr $num1 / $num2` ;;
+    esac
+  
+    read -p "Will you stop? " answer
+  
+    if [ $answer = "YES" ] 
+    then 
+      break 
+    fi
+  done
+fi
+
+echo "Result : $total"
+
+
