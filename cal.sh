83e0d5f1473026939d42b0055fdf67750ab4db56
Minju Jeon <mjeon7890@gmail.com>
2023-01-05 23:53:18 +0900

    fix: split into functions

--- cal.sh
+++ cal.sh
@@ -1,44 +1,78 @@
 #!/bin/bash
-
 echo "====== Simple Calculator ======="
+total=0
+regexNumber="^-?[0-9]+$"
+regexOp="^[\+|\-|\*|\/|\%]$"
 
-total=
-read -p "Input Number : " num1
-read -p "Input Operation : " op
-read -p "Input Number : " num2
-
-case $op in
-  '+') total=`expr $num1 + $num2` ;;
-  '-') total=`expr $num1 - $num2` ;;
-  '*') total=`expr $num1 \* $num2` ;;
-  '/') total=`expr $num1 / $num2` ;;
-esac
-
-read -p "Will you stop? " answer
-
-if [ $answer = "NO" ]
-then
+isNumber()
+{
   while true
   do
-    read -p "Input Operation : " op
-    read -p "Input Number : " num2
-    
-    case $op in
-      '+') total=`expr $total + $num2` ;;
-      '-') total=`expr $total - $num2` ;;
-      '*') total=`expr $total \* $num2` ;;
-      '/') total=`expr $total / $num2` ;;
-    esac
-  
-    read -p "Will you stop? " answer
-  
-    if [ $answer = "YES" ] 
-    then 
-      break 
+    read -p "Input Number : " num
+    if [[ $num =~ $regexNumber ]]
+    then
+      break
+    fi
+    echo "Not a number"
+  done
+  echo ${num}
+}
+
+isOp()
+{
+  while true
+  do
+    read -p "Input Operation (+, -, *, /, %) : " op
+    if [[ $op =~ $regexOp ]]
+    then
+      break
+    fi
+    echo "Not an Operation" 
+  done
+  echo ${op}
+}
+
+cal() 
+{
+echo "op: $1 / num1: $2 / num2: $3"
+case $1 in
+  "+") total=`expr $2 + $3` ;;
+  "-") total=`expr $2 - $3` ;;
+  "*") total=$(($2 * $3)) ;;
+  "/") total=`expr $2 / $3` ;;
+  "%") total=`expr $2 % $3` ;;
+esac
+}
+
+isAnswer()
+{
+  while true
+  do
+    read -p "Will you stop? (YES, NO) : " answer
+    if [ $answer = "YES" -o $answer = "NO" ]
+    then
+      break
     fi
   done
-fi
+  echo ${answer}
+}  
+
+#main
+num1=$(isNumber)
+op=$(isOp)
+num2=$(isNumber)
+cal $op $num1 $num2
+
+while true
+do
+  answer=$(isAnswer)
+  if [ $answer = "YES" ]
+  then
+    break
+  fi
+  op=$(isOp)
+  num2=$(isNumber)
+  cal $op $total $num2
+done
 
 echo "Result : $total"
-
-
